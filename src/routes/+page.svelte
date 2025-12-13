21a3ac37f5efd9a03bb4b223f7635ed9da891228
Nico Smith <web@niiccoo2.xyz>
2025-12-12 23:16:42 -0500

    Add simple video

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import * as tf from '@tensorflow/tfjs';
+	import * as mobilenet from '@tensorflow-models/mobilenet';
+	import { onMount } from 'svelte';
+
+	let video: HTMLVideoElement;
+
+	onMount(async () => {
+		const stream = await navigator.mediaDevices.getUserMedia({
+			video: {
+				width: 640,
+				height: 480
+			}
+		});
+
+		video.srcObject = stream;
+		await video.play();
+	});
+</script>
+
+<video bind:this={video} autoplay playsinline style="transform: scaleX(-1);"></video>
