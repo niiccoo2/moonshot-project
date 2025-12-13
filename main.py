4a6c96f56177fc2e93e6393071dae2d0e9a258a3
Stefanos <165141357+Stefanos0710@users.noreply.github.com>
2025-12-13 14:35:49 +0100

    adbody keypoints display to UI and info extraction

--- main.py
+++ main.py
@@ -7,6 +7,37 @@ from tf.recognision import main
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode="eventlet")
 
+def get_info(result, name):
+    # hands
+    hand_left_fingers = result["hands"]["left"]["fingers"]
+    hand_left_gesture = result["hands"]["left"]["gesture"]
+    hand_left_wrist_x = result["hands"]["left"]["wrist"]["x"]
+    hand_left_wrist_y = result["hands"]["left"]["wrist"]["y"]
+
+    hand_right_fingers = result["hands"]["right"]["fingers"]
+    hand_right_gesture = result["hands"]["right"]["gesture"]
+    hand_right_wrist_x = result["hands"]["right"]["wrist"]["x"]
+    hand_right_wrist_y = result["hands"]["right"]["wrist"]["y"]
+
+    # body
+    body_head_x = result["body"]["head"]["x"]
+    body_head_y = result["body"]["head"]["y"]
+
+    body_shoulder_left_x = result["body"]["left_shoulder"]["x"]
+    body_shoulder_left_y = result["body"]["left_shoulder"]["y"]
+
+    body_shoulder_right_x = result["body"]["right_shoulder"]["x"]
+    body_shoulder_right_y = result["body"]["right_shoulder"]["y"]
+
+    body_elbow_left_x = result["body"]["left_elbow"]["x"]
+    body_elbow_left_y = result["body"]["left_elbow"]["y"]
+
+    body_elbow_right_x = result["body"]["right_elbow"]["x"]
+    body_elbow_right_y = result["body"]["right_elbow"]["y"]
+
+    return locals().get(name)
+
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -19,8 +50,9 @@ def handle_frame(blob):
     result = main(frame)
     emit("result", result)
 
-    # # debug for now
-    # print(result)
+    print(get_info(result, "hand_left_fingers"))
+
+
 
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=5000)
