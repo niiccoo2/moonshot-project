56c9b432358e5f8ce1b7aa74fd4d35e1e4f56707
Nico Smith <web@niiccoo2.xyz>
2025-12-14 22:42:36 -0500

    Fix build settings

--- svelte/svelte.config.js
+++ svelte/svelte.config.js
@@ -1,12 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter({
+			out: 'build'
+		})
+	}
 };
 
 export default config;
